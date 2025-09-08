54124a4acff3e9e67bd81efbd41916972f0110d4
yawlhead91 <alexincork91@gmail.com>
2025-09-08 09:41:38 +0100

    Remove vendor; use Go modules

--- vendor/github.com/teamwork/twapi-go-sdk/internal/browser/browser_darwin.go
+++
@@ -1,5 +0,0 @@
-package browser
-
-func openBrowser(url string, runCmd func(program string, args ...string) error) error {
-	return runCmd("open", url)
-}
--- vendor/github.com/teamwork/twapi-go-sdk/internal/browser/browser_windows.go
+++
@@ -1,9 +0,0 @@
-package browser
-
-import (
-	"golang.org/x/sys/windows"
-)
-
-func openBrowser(url string, runCmd func(program string, args ...string) error) error {
-	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
-}
